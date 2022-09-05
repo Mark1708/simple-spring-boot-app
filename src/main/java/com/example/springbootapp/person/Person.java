b32d53f4761bd270a411880e88aaf3575d13c78e
Марк Гурьянов <mark1708.work@gmail.com>
2022-09-06 00:36:36 +0300

    update with test

--- src/main/java/com/example/springbootapp/person/Person.java
+++ src/main/java/com/example/springbootapp/person/Person.java
@@ -15,4 +15,5 @@ public class Person {
 
     private String name;
     private String role;
+
 }
--- src/test/java/com/example/springbootapp/person/PersonServiceTest.java
+++ src/test/java/com/example/springbootapp/person/PersonServiceTest.java
@@ -1,47 +1,51 @@
-package com.example.springbootapp;
+package com.example.springbootapp.person;
 
-import com.example.springbootapp.person.NotFoundException;
-import com.example.springbootapp.person.Person;
-import com.example.springbootapp.person.PersonRepository;
-import com.example.springbootapp.person.PersonService;
+import com.example.springbootapp.utils.PersonServiceUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static com.example.springbootapp.PersonServiceTestUtil.PERSON_1;
-import static com.example.springbootapp.PersonServiceTestUtil.PERSON_2;
+import static com.example.springbootapp.utils.PersonServiceUtils.PERSON_1;
+import static com.example.springbootapp.utils.PersonServiceUtils.PERSON_2;
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
-class PersonServiceTests {
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:application.properties")
+class PersonServiceTest {
 
-    @Mock
+    @MockBean
     private PersonRepository repo;
 
+    @Autowired
     private PersonService service;
 
     private List<Person> persons;
 
-
     @BeforeEach
-    public void init() {
-        service = new PersonService(repo);
-        PersonServiceTestUtil.initData();
+    void init() {
+        PersonServiceUtils.initData();
         persons = new ArrayList<>();
         persons.add(PERSON_1);
         persons.add(PERSON_2);
-        repo.saveAll(persons);
+        Mockito.when(repo.findPersonById(1L)).thenReturn(Optional.of(PERSON_1));
+        Mockito.when(repo.findPersonById(2L)).thenReturn(Optional.empty());
     }
 
     @Test
-    public void findAllPersonsTest() {
+    void getAllPerson() {
         Mockito.when(repo.findAll()).thenReturn(persons);
         List<Person> result = service.getAllPerson();
         assertEquals(2, result.size());
@@ -50,16 +54,13 @@ class PersonServiceTests {
     }
 
     @Test
-    public void findPersonByIdTest() {
-        Mockito.when(repo.findPersonById(1L)).thenReturn(Optional.of(PERSON_1));
+    void getPerson() {
         assertEquals(PERSON_1, service.getPerson(1L));
     }
 
     @Test
-    public void findPersonByIdExceptionTest() {
+    void getPersonException() {
         Mockito.when(repo.findPersonById(2L)).thenReturn(Optional.empty());
         assertThrows(NotFoundException.class, () -> service.getPerson(2L));
     }
-
-
 }
